d76a81080c500683f93753b31873312c2552b25c
Jake Demarest-Mays <jake@demarest-mays.com>
2026-07-26 07:58:01 -0600

    Initial commit.

---
+++ lib/ex_trafilatura.ex
@@ -0,0 +1,18 @@
+defmodule ExTrafilatura do
+  @moduledoc """
+  Documentation for `ExTrafilatura`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExTrafilatura.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_trafilatura_test.exs
@@ -0,0 +1,8 @@
+defmodule ExTrafilaturaTest do
+  use ExUnit.Case
+  doctest ExTrafilatura
+
+  test "greets the world" do
+    assert ExTrafilatura.hello() == :world
+  end
+end
